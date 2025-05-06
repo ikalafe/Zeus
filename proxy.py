cd94894a913b73aebcd8deade817aa8ddf26127b
ikalafe <imkalafe@gmail.com>
2025-05-06 00:55:08 -0400

    Add server loop function to manage incoming client connections
    This commit introduces the server_loop function in proxy.py, which sets up a TCP server to listen for incoming connections on a specified local host and port. It handles client connections by spawning a new thread for each connection, allowing for concurrent processing. This enhancement builds upon the existing proxy_handler function, improving the overall functionality of the proxy script.

--- proxy.py
+++ proxy.py
@@ -34,6 +34,30 @@ def receive_from(connection):
         pass
     return buffer
 
+def server_loop(local_host, local_port, remote_host, remote_port, receive_first):
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # 1
+    try:
+        server.bind((local_host, local_port)) # 2
+    except Exception as e:
+        print("problem on bind: %r" % e)
+
+        print("[!!] Failed to listen on %s:%d" % (local_host, local_port))
+        print("[!!] Check for other listening sockets or correct permissions.")
+        sys.exit(0)
+
+    print("[*] Listening on %s:%d" % (local_host, local_port))
+    server.listen(5)
+    while True: # 3
+        client_socket, addr = server.accept
+        # print out the local connection information
+        line = "> Received incoming connection from %s:%d" % (addr[0], addr[1])
+        print(line)
+        # start a thread to talk to the remote host
+        proxy_thread = threading.Thread(# 3
+            target=proxy_handler,
+            args=(client_socket,remote_host,remote_port, receive_first))
+        proxy_thread.start()
+
 def proxy_handler(client_socket, remote_host, remote_port, receive_first):
     remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     remote_socket.connect((remote_host, remote_port))
@@ -54,7 +78,7 @@ def proxy_handler(client_socket, remote_host, remote_port, receive_first):
             print(line)
             hexdump(local_buffer)
             
-            local_buffer = request_handler
+            local_buffer = request_handler(local_buffer)
             remote_socket.send(local_buffer)
             print("[==>] Send to remote.")
 
