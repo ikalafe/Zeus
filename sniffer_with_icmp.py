966ad98ff8d551a9817504f37efed7ec46274ff4
ikalafe <imkalafe@gmail.com>
2025-05-26 10:04:28 -0400

    Enhance ICMP packet sniffer with UDP sender and subnet configuration
    This commit updates the sniffer_with_icmp.py script to include a UDP sender function that broadcasts a magic message across a specified subnet. Additionally, it refines the ICMP packet sniffing functionality by encapsulating it within a Scanner class, improving code organization and readability. The script now allows for dynamic subnet targeting and better handling of ICMP packets.

--- sniffer_with_icmp.py
+++ sniffer_with_icmp.py
@@ -6,6 +6,11 @@ import sys
 import threading
 import time
 
+# subnet to target
+SUBNET = '192.168.22.195'
+# magin string we'll check ICMP response for 
+MESSAGE = 'PYTHONRULES!'
+
 class IP:
     def __init__(self, buff=None):
         header = struct.unpack('!BBHHHBBH4s4s', buff)
@@ -34,54 +39,78 @@ class IP:
             print('%s No protocol for %s' % (e, self.protocol_num))
 
 class ICMP:
-    def __init__(self, buff):
-        header = struct.unpack('BBHHH', buff)
+    def __init__(self, buf):
+        header = struct.unpack('BBHHH', buf)
         self.type = header[0]
         self.code = header[1]
         self.sum = header[2]
         self.id = header[3]
         self.seq = header[4]
 
-def sniff(host):
-    # should look familiar from previeus example
-    if os.name == "nt":
-        socket_protocol = socket.IPPROTO_IP
-    else:
-        socket_protocol = socket.IPPROTO_ICMP
+# this sprays out UDP daragrams with our magic message
+def udp_sender():
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sender:
+        for ip in ipaddress.ip_network(SUBNET).hosts():
+            sender.sendto(bytes(MESSAGE, 'utf8'), (str(ip), 65212))
 
-    sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
-    sniffer.bind((host, 0))
-    sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)      
-
-    if os.name == 'nt':
-        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
-
-    try:
-        while True:
-            # read a packet
-            raw_buffer = sniffer.recvfrom(65535)[0]
-            ip_header = IP(raw_buffer[0:20])
-            if ip_header.protocol == "ICMP":
-                # create an IP header from the first 20 bytes
-                # print the detected protocol and host
-                print('Protocol: %s %s -> %s' % (ip_header.protocol, ip_header.src_address, ip_header.dst_address))
-                print(f'Version: {ip_header.ver}')
-                print(f'Header: {ip_header.ihl} TTL: {ip_header.ttl}')
-
-                # calculate where our ICMP packet starts
-                offset = ip_header.ihl * 4
-                buf = raw_buffer[offset:offset + 8]
-                # create our ICMP structure
-                icmp_header = ICMP(buff=buf)
-                print('ICMP -> Type: %s Code: %s\n' % (icmp_header.type, icmp_header.code))
-    except KeyboardInterrupt:
-        # if we're on Windows, true off promiscuous mode
+class Scanner:
+    def __init__(self, host):
+        self.host = host
         if os.name == 'nt':
-            sniffer.ioctl(socket.SIP_RCVALL, socket.RCV_OFF)
-        sys.exit()
+            socket_protocol = socket.IPPROTO_IP
+        else:
+            socket_protocol = socket.IPPROTO_ICMP
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+
+        self.socket.bind((host,0))
+
+        self.socket.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+
+        if os.name == 'nt': 
+            self.socket.ioct(socket.SIO_RCVALL, socket.RCVALL_ON)
+    def sniff(self):
+        # should look familiar from previeus example
+        if os.name == "nt":
+            socket_protocol = socket.IPPROTO_IP
+        else:
+            socket_protocol = socket.IPPROTO_ICMP
+
+        sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+        sniffer.bind((self.host, 0))
+        sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)      
+
+        if os.name == 'nt':
+            sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+
+        host_up = set([f'{str(self.host)} *'])
+        try:
+            while True:
+                # read a packet 
+                raw_buffer = self.socket.recvfrom(65535)[0]
+                # create an IP header from the first 20 bytes
+                ip_header = IP(raw_buffer[0:20])
+                # if it's ICMP, we want it
+                if ip_header.protocol == "ICMP":
+                    print('Protocol: %s %s -> %s' % (ip_header.protocol, ip_header.src_address, ip_header.dst_address))
+                    print(f'Version: {ip_header.ver}')
+                    print(f'Header Length: {ip_header.ihl} TTL: {ip_header.ttl}')
+
+                    # calculate where our ICMP packet starts
+                    offset = ip_header.ihl * 4
+                    buf = raw_buffer[offset:offset + 8]
+                    # create our ICMP structures
+                    icmp_header = ICMP(buf=buf)
+                    print('ICMP -> Type: %s Code: %s\n' % (icmp_header.type, icmp_header.code))
+        except KeyboardInterrupt:
+            if os.name == 'nt':
+                sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+            sys.exit()
+
 if __name__ == '__main__':
     if len(sys.argv) == 2:
         host = sys.argv[1]
     else:
         host = '192.168.84.135'
-    sniff(host=host)
+    scanner = Scanner(host)
+    scanner.sniff()
+        
