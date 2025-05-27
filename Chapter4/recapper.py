fb616c39e09c41183fb2c94a2fc9f4bc25860322
ikalafe <imkalafe@gmail.com>
2025-05-27 17:13:35 -0400

    Add recapper.py for extracting and processing image content from pcap files

---
+++ Chapter4/recapper.py
@@ -0,0 +1,43 @@
+from scapy.all import TCP, rdpcap
+import collections
+import os
+import re
+import sys
+import zlib
+
+OUTDIR = '/root/Desktop/picture'
+PCAPS = '/root/Downloads'
+
+Response = collections.namedtuple('Response', ['header', 'payload'])
+
+def get_header(payload):
+    try:
+        header_raw = payload[:payload.index(b'\r\n\r\n') + 2]
+    except ValueError:
+        sys.stdout.write('-')
+        sys.stdout.flush()
+        return None
+    
+    header = dict(re.findall(r'(?P<name>.*?): (?P<value>.*?)\r\n', header_raw.decode()))
+    if 'Content-Type' not in header:
+        return None
+    return header
+
+def extract_content(Response, content_name='image'):
+    pass
+
+class Recapper:
+    def __init__(self, fname):
+        pass
+    
+    def get_response(self):
+        pass
+    
+    def write(self, content_name):
+        pass
+    
+if __name__ == '__main__':
+    pfile = os.path.join(PCAPS, 'pcap.pcap')
+    recapper = Recapper(pfile)
+    recapper.get_response()
+    recapper.write('image')
