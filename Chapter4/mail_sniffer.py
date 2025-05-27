86c467ecdbfc4add8412bc63818d1a8a6a2917ad
ikalafe <imkalafe@gmail.com>
2025-05-27 10:49:55 -0400

    Remove deprecated network utility scripts and RSA key files
    This commit deletes several Python scripts related to network utilities, including netcat.py, proxy.py, scan.py, scanner.py, and various SSH-related scripts. Additionally, it removes RSA key files (rsa.key and rsa.key.pub) that are no longer needed. This cleanup enhances the project by eliminating unused code and files, streamlining the repository for future development.

---
+++ Chapter4/mail_sniffer.py
@@ -0,0 +1,16 @@
+from scapy.all import sniff, TCP, IP
+
+# the packet callback
+def packet_callback(packet):
+    if packet[TCP].payload:
+        mypacket = str(packet[TCP].payload)
+    if 'user' in mypacket.lower() or 'pass' in mypacket.lower():
+        print(f"[*] Destination: {packet[IP].dst}")
+        print(f"[*] {str(packet[TCP].payload)}")
+
+def main():
+    # firt up the sniffer
+    sniff(filter='tcp port 110 or tcp port 25 or tcp port 443',prn=packet_callback, store=0)
+
+if __name__ == '__main__':
+    main()
