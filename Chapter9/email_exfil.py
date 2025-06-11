b83f7534a958a9115cb790140b2c4c338bd70afa
ikalafe <imkalafe@gmail.com>
2025-06-11 13:39:14 -0400

    feat: create email_exfil.py

---
+++ Chapter9/email_exfil.py
@@ -0,0 +1,9 @@
+import smtplib
+import time
+import win32com.client
+
+smtp_server = 'smtp.example.com'
+smtp_port = 587
+smtp_acct = 'imkalafe@gmail.com'
+smtp_password = 'secret'
+tgt_accts = ['daniyaldehghan2021@gmail.com']
